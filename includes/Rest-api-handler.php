6e8de7ccb67c3b0873f7f9d9eba63473beb3b739
Rihnoset123 <rihnoset123@proton.me>
2024-05-26 21:12:23 +0200

    Procedural -> OOP

--- includes/Rest-api-handler.php
+++ includes/Rest-api-handler.php
@@ -1,80 +1,82 @@
 <?php
-class Rest_api {
-    public function __construct() {
-        add_action('rest_api_init',array($this,'register_rest_api'));
-        error_log("uspio");
-    }
-    public function register_rest_api()
-    {
-        register_rest_route(
-            'staking-pool',
-            'stake',
-            array(
-                'methods' => 'GET, POST',
-                'callback' => array($this,'handle_rest_api_reqs'),
-                'permission_callback' => '__return_true'
+if (!class_exists('Rest_api'))
+{
+    class Rest_api {
+        public function __construct() {
+            add_action('rest_api_init',array($this,'register_rest_api'));
+            error_log("uspio");
+        }
+        public function register_rest_api()
+        {
+            register_rest_route(
+                'staking-pool',
+                'stake',
+                array(
+                    'methods' => 'GET, POST',
+                    'callback' => array($this,'handle_rest_api_reqs'),
+                    'permission_callback' => '__return_true'
 
-            )
-        );
+                )
+            );
 
-    }
-
-    public function handle_rest_api_reqs($request)
-    {
-        global $stakePlugin;
-        $headers = $request->get_headers();
-        $action = $headers['action'];
-        switch ($action[0]) {
-            case 'payment_request':
-
-                try {
-                $stakeAmmount = (int)$headers['stakeammount'][0];
-                error_log($stakeAmmount);
-                $stakePlugin->xaman_payment_req($stakeAmmount);
-
-                } catch (Exception $e) {
-                    $error_message = $e->getMessage();
-                    error_log($error_message);
-                    echo json_encode($error_message);
-                }
-                break;
-
-            case 'checking_transaction':
-
-                try {
-                    $stakeAmmount = (int)$headers['stakeammount'][0];
-                    $stakepercentage = (int)$headers['stakepercentage'][0];
-                    $duration = (int)$headers['duration'][0];
-                    error_log("stakeAmmount: ".$stakeAmmount);
-                    error_log("stake: ". $stakeAmmount);
-                    $uuid = $headers['uuid'][0];
-                    $client = new \GuzzleHttp\Client();
-                    $url = "https://xumm.app/api/v1/platform/payload/" . $uuid;
-                    $response = $client->request('GET', $url, [
-                        'headers' => [
-                            'X-API-Key' => get_option("XUMM_KEY"),
-                            'X-API-Secret' => get_option("XUMM_SECRET"),
-                            'accept' => 'application/json',
-                        ],
-                    ]);
-                $body = json_decode($response->getBody(), true);
-                $staker = $body['response']['account'];
-                
-                $stakePlugin->schedule_stk_payout($duration,$stakeAmmount,$staker,$stakepercentage);
-                return wp_send_json_success($body);
-        
-        
-                } catch (Exception $e) {
-                    $error_message = $e->getMessage();
-                    error_log($error_message);
-                    echo json_encode($error_message);
-                }
-                break;
-            default:
-            error_log("Not valid request!");
         }
 
+        public function handle_rest_api_reqs($request)
+        {
+            global $stakePlugin;
+            $headers = $request->get_headers();
+            $action = $headers['action'];
+            switch ($action[0]) {
+                case 'payment_request':
+
+                    try {
+                    $stakeAmmount = (int)$headers['stakeammount'][0];
+                    error_log($stakeAmmount);
+                    $stakePlugin->xaman_payment_req($stakeAmmount);
+
+                    } catch (Exception $e) {
+                        $error_message = $e->getMessage();
+                        error_log($error_message);
+                        echo json_encode($error_message);
+                    }
+                    break;
+
+                case 'checking_transaction':
+
+                    try {
+                        $stakeAmmount = (int)$headers['stakeammount'][0];
+                        $stakepercentage = (int)$headers['stakepercentage'][0];
+                        $duration = (int)$headers['duration'][0];
+                        error_log("stakeAmmount: ".$stakeAmmount);
+                        error_log("stake: ". $stakeAmmount);
+                        $uuid = $headers['uuid'][0];
+                        $client = new \GuzzleHttp\Client();
+                        $url = "https://xumm.app/api/v1/platform/payload/" . $uuid;
+                        $response = $client->request('GET', $url, [
+                            'headers' => [
+                                'X-API-Key' => get_option("XUMM_KEY"),
+                                'X-API-Secret' => get_option("XUMM_SECRET"),
+                                'accept' => 'application/json',
+                            ],
+                        ]);
+                    $body = json_decode($response->getBody(), true);
+                    $staker = $body['response']['account'];
+                    
+                    $stakePlugin->schedule_stk_payout($duration,$stakeAmmount,$staker,$stakepercentage);
+                    return wp_send_json_success($body);
+            
+            
+                    } catch (Exception $e) {
+                        $error_message = $e->getMessage();
+                        error_log($error_message);
+                        echo json_encode($error_message);
+                    }
+                    break;
+                default:
+                error_log("Not valid request!");
+            }
+
+        }
     }
 }
-
 new Rest_api();
--- xrpl-staking-pool.php
+++ xrpl-staking-pool.php
@@ -25,127 +25,130 @@ if (!function_exists('add_action')) {
 const MAINNET_URL=  'https://xrplcluster.com/';
 const TESTNET_URL = 'https://testnet.xrpl-labs.com/';
 define('SECONDS_IN_MONTH', 30 * 24 * 60 * 60);
-class StakePlugin {
-    private $xummSdk;
-    public function __construct() {
-        $this->initHooks();
-        $this->xummSdk = new XummSdk(get_option('XUMM_KEY'), get_option('XUMM_SECRET'));
-        error_log("uspio");
-    }
+    class StakePlugin {
+        private $xummSdk;
+        public function __construct() {
+            $this->initHooks();
+            $this->xummSdk = new XummSdk(get_option('XUMM_KEY'), get_option('XUMM_SECRET'));
+            error_log("uspio");
+        }
 
-    private function initHooks() {
-         add_action('stake_payment', array($this,'sending_tokens'), 10, 3);
-         add_action('wp_enqueue_scripts', array($this, 'enqueue_custom_scripts'));
-         require_once plugin_dir_path(__FILE__) . 'includes/jQuery-handler.php';
-         require_once plugin_dir_path(__FILE__) . 'includes/Settings.php';
-         require_once plugin_dir_path(__FILE__) . 'includes/Rest-api-handler.php';
-     }
+        private function initHooks() {
+            add_action('stake_payment', array($this,'sending_tokens'), 10, 3);
+            add_action('wp_enqueue_scripts', array($this, 'enqueue_custom_scripts'));
+            require_once plugin_dir_path(__FILE__) . 'includes/jQuery-handler.php';
+            require_once plugin_dir_path(__FILE__) . 'includes/Settings.php';
+            require_once plugin_dir_path(__FILE__) . 'includes/Rest-api-handler.php';
+        }
 
-    public function enqueue_custom_scripts() {
-        wp_enqueue_script('jquery');
-    }
-    public function xaman_payment_req($stakeAmmount) {
-        $secretKey = get_option('secret_key'); 
-        $recipientAddress = Wallet::fromSeed($secretKey);
+        public function enqueue_custom_scripts() {
+            wp_enqueue_script('jquery');
+        }
+        public function xaman_payment_req($stakeAmmount) {
+            $secretKey = get_option('secret_key'); 
+            $recipientAddress = Wallet::fromSeed($secretKey);
 
-        $payload = new Payload([
-            'TransactionType' => 'Payment',
-            'Destination' => $recipientAddress->getClassicAddress(),
-            'Amount' => [
-                'currency' => get_option('token_name'),
-                'value' => $stakeAmmount,
-                'issuer' => get_option('issuer_key')
-            ] 
-        ]);
+            $payload = new Payload([
+                'TransactionType' => 'Payment',
+                'Destination' => $recipientAddress->getClassicAddress(),
+                'Amount' => [
+                    'currency' => get_option('token_name'),
+                    'value' => $stakeAmmount,
+                    'issuer' => get_option('issuer_key')
+                ] 
+            ]);
 
-            try {
-                $response = $this->xummSdk->createPayload($payload);
-                if (isset($response->uuid)) {
-                    header('Content-Type: application/json');
-                    echo json_encode([
-                        'uuid' => $response->uuid, 
-                        'websocket' => $response->refs->websocketStatus,
-                        'redirect_url' => $response->next->always
-                    ]);
-                } else {
-                    echo json_encode(['success' => false, 'message' => 'Failed to create payload']);
+                try {
+                    $response = $this->xummSdk->createPayload($payload);
+                    if (isset($response->uuid)) {
+                        header('Content-Type: application/json');
+                        echo json_encode([
+                            'uuid' => $response->uuid, 
+                            'websocket' => $response->refs->websocketStatus,
+                            'redirect_url' => $response->next->always
+                        ]);
+                    } else {
+                        echo json_encode(['success' => false, 'message' => 'Failed to create payload']);
+                        return null;
+                    }
+                } catch (Exception $e) {
+                    error_log('Failed to create sign-in payload: ' . $e->getMessage());
                     return null;
                 }
-            } catch (Exception $e) {
-                error_log('Failed to create sign-in payload: ' . $e->getMessage());
-                return null;
+        }
+
+        public function schedule_stk_payout($duration,$amount, $staker, $stake) {
+            
+            if(get_option("enable_test"))
+            {
+                wp_schedule_single_event(time() + $duration, 'stake_payment', [$amount, $staker, $stake]);
             }
-    }
+            else 
+            {
+                wp_schedule_single_event(time() + $duration * SECONDS_IN_MONTH, 'stake_payment', [$amount, $staker, $stake]);
+            }
+        }
 
-    public function schedule_stk_payout($duration,$amount, $staker, $stake) {
-        
-        if(get_option("enable_test"))
+
+
+        public function sending_tokens( $amount, $staker, $stake)
         {
-            wp_schedule_single_event(time() + $duration, 'stake_payment', [$amount, $staker, $stake]);
-        }
-        else 
-        {
-            wp_schedule_single_event(time() + $duration * SECONDS_IN_MONTH, 'stake_payment', [$amount, $staker, $stake]);
-        }
-    }
+            $secretKey = get_option('secret_key'); 
+            $recipientAddress = Wallet::fromSeed($secretKey);
+            $issuer_address = get_option('issuer_key');
+            $amountToSend = $this->calculate_amount($amount,$stake);
+            $transaction = $this->prepare_transaction($recipientAddress->getClassicAddress(), $staker, $amountToSend, $issuer_address);
 
+            if($amountToSend > 0)
+            {
+                $this->sign_transaction($transaction, $secretKey);
+            }
 
-
-    public function sending_tokens( $amount, $staker, $stake)
-    {
-        $secretKey = get_option('secret_key'); 
-        $recipientAddress = Wallet::fromSeed($secretKey);
-        $issuer_address = get_option('issuer_key');
-        $amountToSend = $this->calculate_amount($amount,$stake);
-        $transaction = $this->prepare_transaction($recipientAddress->getClassicAddress(), $staker, $amountToSend, $issuer_address);
-
-        if($amountToSend > 0)
-        {
-            $this->sign_transaction($transaction, $secretKey);
+            wp_die(); 
         }
 
-        wp_die(); 
-    }
 
 
-
-    public function calculate_amount($ammount,$stake) {
-        $ammountToSend = $ammount * $stake/100 + $ammount;
-        return $ammountToSend;
-    }
-
-    public function prepare_transaction($fromAddress, $toAddress, $amount,$issuer_address) {
-
-        $transaction = [
-            'TransactionType' => 'Payment',
-            'Account' => $fromAddress,
-            'Destination' => $toAddress,
-            'Amount' => [ 
-                'currency' => get_option('token_name'),
-                'value' => $amount,
-                'issuer' => $issuer_address
-            ], 
-        ];
-        return json_encode($transaction);
-    }
-
-    public function sign_transaction($transaction, $secretKey) {
-        
-        $client = new JsonRpcClient(TESTNET_URL);
-        $standbyWallet = Wallet::fromSeed($secretKey);
-        $tx = json_decode($transaction,true);
-        $autofilledTx = $client->autofill($tx);
-        $signedTx = $standbyWallet->sign($autofilledTx);
-        $txResponse = $client->submitAndWait($signedTx['tx_blob']);
-        $result = $txResponse->getResult();
-        if ($result['meta']['TransactionResult'] === 'tecUNFUNDED_PAYMENT') {
-            error_log("Error: The sending account is unfunded! TxHash: {$result['hash']}" . PHP_EOL);
-        } else {
-            error_log("Token payment done! TxHash: {$result['hash']}" . PHP_EOL);
+        public function calculate_amount($ammount,$stake) {
+            $ammountToSend = $ammount * $stake/100 + $ammount;
+            return $ammountToSend;
         }
 
-    }   
-}
+        public function prepare_transaction($fromAddress, $toAddress, $amount,$issuer_address) {
 
-global $stakePlugin;
-$stakePlugin = new StakePlugin();
+            $transaction = [
+                'TransactionType' => 'Payment',
+                'Account' => $fromAddress,
+                'Destination' => $toAddress,
+                'Amount' => [ 
+                    'currency' => get_option('token_name'),
+                    'value' => $amount,
+                    'issuer' => $issuer_address
+                ], 
+            ];
+            return json_encode($transaction);
+        }
+
+        public function sign_transaction($transaction, $secretKey) {
+            
+            $client = new JsonRpcClient(TESTNET_URL);
+            $standbyWallet = Wallet::fromSeed($secretKey);
+            $tx = json_decode($transaction,true);
+            $autofilledTx = $client->autofill($tx);
+            $signedTx = $standbyWallet->sign($autofilledTx);
+            $txResponse = $client->submitAndWait($signedTx['tx_blob']);
+            $result = $txResponse->getResult();
+            if ($result['meta']['TransactionResult'] === 'tecUNFUNDED_PAYMENT') {
+                error_log("Error: The sending account is unfunded! TxHash: {$result['hash']}" . PHP_EOL);
+            } else {
+                error_log("Token payment done! TxHash: {$result['hash']}" . PHP_EOL);
+            }
+
+        }   
+    }
+    
+    global $stakePlugin;
+    $stakePlugin = new StakePlugin();
+
+
+
